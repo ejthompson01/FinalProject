684f64a49a943acb7055e0831852f733ed19b8f2
Elizabeth Thompson <ejthompson1313@gmail.com>
2025-05-12 11:18:31 -0400

    adding files to repository

---
+++ pattern.py
@@ -0,0 +1,182 @@
+from fpdf import FPDF
+import re
+from pathlib import Path
+
+class SweaterPattern:
+    def __init__(self, name, neck, sleeve, embellishment,
+                    rib1, rib2, cuff, collar, size,
+                    sleeve_length, sleeve_cuff_width, shoulder_width,
+                    body_height, bottom_width, neck_opening_width,
+                    length, stitch, height, row):
+        self.sleeve = sleeve
+        self.neck = neck
+        self.embellishment = embellishment
+        self.size = size
+        self.length = length
+        self.stitch = stitch
+        self.height = height
+        self.row = row
+        self.sleeve_length = sleeve_length
+        self.sleeve_cuff_width = sleeve_cuff_width
+        self.shoulder_width = shoulder_width
+        self.body_height = body_height
+        self.bottom_width = bottom_width
+        self.neck_opening_width = neck_opening_width
+        self.name = name
+        self.rib1 = rib1
+        self.rib2 = rib2
+        self.cuff = cuff
+        self.collar = collar
+        self.pattern = ''
+
+        self.stitch_gauge = round(stitch/length)
+        self.row_gauge = round(row/height)
+
+        size_chart = {
+            "Small": {'sleeve_length': 19, 'sleeve_cuff_width': 3.5, 'shoulder_width': 23, "body_height": 24.5,
+                      "bottom_width": 21, "neck_opening_width": 14, "sleeve_opening_width": 7.5, "mock_height": 2, 'turtle_height': 6},
+            "Medium": {"sleeve_length": 19.5, "sleeve_cuff_width": 3.75, "shoulder_width": 23, "body_height": 25.5,
+                       "bottom_width": 22, "neck_opening_width": 15, "sleeve_opening_width": 8, 'mock_height': 2, 'turtle_height': 6},
+            "Large": {"sleeve_length": 20, "sleeve_cuff_width": 4, "shoulder_width": 25, "body_height": 26.5,
+                      "bottom_width": 23, "neck_opening_width": 16, "sleeve_opening_width": 8.5, 'mock_height': 2.5, 'turtle_height': 6},
+            "X-Large": {"sleeve_length": 21, "sleeve_cuff_width": 4.25, "shoulder_width": 26, "body_height": 27.5,
+                        "bottom_width": 24, "neck_opening_width": 17, "sleeve_opening_width": 9, 'mock_height': 2.5, 'turtle_height': 6}
+        }
+        if self.size != "Insert own measurements":
+            self.sleeve_length = size_chart[self.size]["sleeve_length"]
+            self.sleeve_cuff_width = size_chart[self.size]["sleeve_cuff_width"]
+            self.shoulder_width = size_chart[self.size]["shoulder_width"]
+            self.body_height = size_chart[self.size]["body_height"]
+            self.bottom_width = size_chart[self.size]["bottom_width"]
+            self.neck_opening_width = size_chart[self.size]["neck_opening_width"]
+            self.sleeve_opening_width = size_chart[self.size]["sleeve_opening_width"]
+            if self.neck == 'Mock neck':
+                self.collar = size_chart[self.size]['mock_height']
+            if self.neck == 'Turtleneck':
+                self.collar = size_chart[self.size]['turtle_height']
+        if self.size == 'Insert own measurements':
+            self.size == 'custom'
+        
+        if self.sleeve == 'Tapered' or 'Balloon':
+            self.num_decreases = round(((self.sleeve_opening_width*2) - (self.sleeve_cuff_width*2))*self.stitch_gauge/2)
+            num_rounds = self.sleeve_length * self.row_gauge
+            self.decrease = round(num_rounds/self.num_decreases)
+        if self.sleeve == 'Flare':
+            self.num_increases = round(((self.sleeve_cuff_width*2) - (self.sleeve_opening_width*2))*self.stitch_gauge/2)
+            num_rounds = self.sleeve_length * self.row_gauge
+            self.increase = round(num_rounds/self.num_increases)
+        
+
+    def layout(self):
+        """
+        Contructs the text of the pattern.        
+        """
+        # Variables for repeated sentences:
+        if self.cuff != None:
+            cuff = f"Knit a {self.rib1}x{self.rib2} rib until your rib measures {self.cuff} inches."
+        else:
+            cuff = ''
+        if self.collar != None:
+            collar = f"Knit a {self.rib1}x{self.rib2} rib until your rib measures {self.collar} inches."
+        else:
+            collar = ''
+
+        # Pattern
+        self.pattern = [
+            # Intro
+            ('h1', f'{self.name} Sweater Pattern'),
+            ('text', f'This is the pattern for your {self.size.lower()}, {self.sleeve.lower()}, {self.neck.lower()}, drop shoulder sweater. This pattern is written using knitting jargon so please look up any terms you are unfamiliar with. Please keep in mind that yarn types may alter some measurements. Consistently test your work against your desired measurements throughout your project to ensure accuracy. Make adjustments if needed. Enjoy!'),
+            ('text', ''),
+            ('h2', f'Materials Needed'),
+            ('text', f'- 1200 to 1800 yards of yarn'),
+            ('text', f'- 9 inch and 32 inch circular needles'),
+            ('text', f'- 1 tapestry needle'),
+            ('text', f'- 1 stitch marker'),
+            ('text', ''),
+
+            # Sweater Body
+            ('h2', f'Knit the Body'),
+            ('text', f'1. Cast on {self.bottom_width*self.stitch_gauge*2} stitches to 32 inch circular needles, connecting them in the round. {cuff}'),
+            ('text', f'2. Knit stockinette until your work measures {round(self.body_height-self.sleeve_opening_width)} inches.'),
+            ('text', f'3. Split for sleeves by putting {self.bottom_width*self.stitch_gauge} stitches onto a separate set of needles or scrap string.')
+        ]
+        if self.neck == 'Boat neck':
+            self.pattern += [
+                ('text', f'4. Knit the front half by continuing to knit stockinette until your work measures {self.sleeve_opening_width} inches from the underarm to the top. {collar}'),
+                ('text', f'5. If you used scrap string, put the back half stitches onto your needles. If you used a separate set of needles for the back half, switch to those.'),
+                ('text', f'6. Knit stockinette until the front and back heights are equal. {collar}')
+                ]
+        else:
+            self.pattern += [
+                ('text', f'4. Knit the front half by continuing to knit stockinette until your work measures {self.sleeve_opening_width} inches from the underarm to the top.'),
+                ('text', f'5. If you used scrap string, put the back half stitches onto your needles. If you used a separate set of needles for the back half, switch to those.'),
+                ('text', f'6. Knit stockinette until the front and back heights are equal.')
+                ]
+        self.pattern += [
+            ('text', f'7. Use a mattress stitch and a tapestry needle to sew the front and back of the top of your work together. Sew {(self.shoulder_width-self.neck_opening_width)/2} inches of stitches together from the left shoulder towards the center. Repeat for the right edge.')
+            ]
+        if self.neck != 'Boat neck':
+            if self.collar != None:
+                self.pattern += [('text', f'8. Pick up the neckline stitches and knit a {self.rib1}x{self.rib2} rib until your rib measures {self.collar} inches.')]
+            else:
+                self.pattern += [('text', f'8. Pick up the neckline stitches and knit stockinette until your collar measures {self.collar} inches.')]
+        # Sleeves
+        self.pattern += [
+            ('text', ''),
+            ('h2', f'Knit the {self.sleeve} Sleeves'),
+            ('text', f'1. Pick up every third stitch on the left arm hole. Place a stitch marker on your right needle to mark the beginning of the row.')
+            ]
+        if self.sleeve == 'Straight':
+            self.pattern += [
+                ('text', f'2. Knit stockinette until your sleeves measure {self.sleeve_length} inches. {cuff}'),
+                ('text', f'3. Cast off.'),
+                ('text', f'4. Repeat steps 1-3 for the right sleeve.')
+                ]
+        if self.sleeve == 'Tapered':
+            self.pattern += [
+                ('text', f'2. Knit {self.decrease-1} rounds of stockinette. Knit another round and K2Tog the last two stitches before the stitch marker. Knit one stitch after the stitch marker then K2Tog the next two stitches. {cuff}'),
+                ('text', f'3. Repeat Step 2 {self.num_decreases} times.'),
+                ('text', f'4. Cast off.'),
+                ('text', f'5. Repeat steps 1-3 for the right sleeve.'),
+                ]
+        if self.sleeve == 'Flare':
+            self.pattern += [
+                ('text', f'2. Knit {self.increase-1} rounds of stockinette. Knit another round and M1R before the stitch marker. Knit one stitch after the stitch marker then M1L. {cuff}'),
+                ('text', f'3. Repeat Step 2 {self.num_increases} times.'),
+                ('text', f'4. Cast off.'),
+                ('text', f'5. Repeat steps 1-3 for the right sleeve.'),
+                ]
+        if self.sleeve == 'Balloon':
+            self.pattern += [
+                ('text', f'2. Knit stockinette until your sleeves measure {self.sleeve_length}. Knit two more rounds and K2Tog every {round(self.sleeve_opening_width*2/self.num_decreases*2)} stitches. {cuff}'),
+                ('text', f'3. Cast off.'),
+                ('text', f'4. Repeat steps 1-3 for the right sleeve.')
+                ]
+
+
+    def generate_pdf(self):
+        self.layout()
+
+        Path("downloads").mkdir(exist_ok=True)
+        safe_name = re.sub(r'[^\w\s-]', '', self.name).strip().replace(' ', '_')
+        filename = f"{safe_name}_sweater_pattern.pdf"
+
+        pdf = FPDF()
+        pdf.add_page()
+
+        for kind, text in self.pattern:
+            if kind == 'h1':
+                pdf.set_fill_color(255, 166, 195)
+                pdf.set_font('Courier', 'BU', 16)
+                pdf.cell(0, 10, text, ln=True, fill=True, align='C')
+            elif kind == 'h2':
+                pdf.cell(0, 3, '', ln=True)
+                pdf.set_fill_color(255, 240, 245)
+                pdf.set_font('Courier', 'B', 14)
+                pdf.cell(0, 10, text, ln=True, fill=True)
+            else:  # default paragraph
+                pdf.set_font('Courier', '', 12)
+                pdf.multi_cell(0, 10, text)
+        
+        pdf.output(filename)
+        return filename
---
+++ src/dashboard.py
@@ -0,0 +1,422 @@
+from dash import Dash, dcc, html, Input, Output, callback
+import pandas as pd
+from PIL import Image
+from pattern import SweaterPattern
+from flask import send_from_directory
+
+df = pd.read_csv('data/knit_data.csv')
+
+def run_app() -> None:
+    app = Dash(__name__)
+    app.title = 'Sweater Dashboard'
+
+    @app.server.route("/download/<filename>")
+    def serve_pdf(filename):
+        return send_from_directory("downloads", filename, as_attachment=True)
+    
+    create_layout(app)
+    app.run(debug=True, port=8051)
+    return None
+
+def create_layout(app: Dash) -> None:
+
+    header = html.H1('Drop Shoulder Sweater Customization Dashboard',
+                     style={'textAlign': 'center',
+                            'fontFamily': 'American Typewriter',
+                            'background-color': '#ffa6c3'}
+                    )
+    name_label = html.H4("Enter the name of your sweater (ex. My Sweater, The Kate, Alex's Sweater):",
+                           style={'fontFamily': 'American Typewriter'}
+                            )
+    
+    name_input = dcc.Input(id='name-input',
+                            type='text',
+                            placeholder='Enter name here',
+                            style={'fontFamily': 'American Typewriter'}
+                            )
+    
+    # COLUMN 1
+    children = []
+    # Neckline (second dropdown menu, select neckline type)
+    neck_heading = html.H2('Neckline',
+                            style={'fontFamily': 'American Typewriter',
+                                'background-color': 'LavenderBlush',
+                                'textAlign': 'center'}
+                            )
+    neck_label = html.H4('Select a neckline type:',
+                        style={'fontFamily': 'American Typewriter'}
+                        )
+    neck_dd = dcc.Dropdown(id='neck-dd',
+                           options=df['neck'].to_list(),
+                           value = 'Boat neck',
+                           style={'fontFamily': 'American Typewriter'}
+                          )
+    children += [neck_heading, neck_label, neck_dd]
+
+
+    # Sleeves (first dropdown menu, select sleeve type)
+    sleeve_heading = html.H2('Sleeve',
+                           style={'fontFamily': 'American Typewriter',
+                                  'background-color': 'LavenderBlush',
+                                  'textAlign': 'center'}
+                            )
+    sleeve_label = html.H4('Select a sleeve type:',
+                           style={'fontFamily': 'American Typewriter'}
+                            )
+    sleeve_dd = dcc.Dropdown(id='sleeve-dd',
+                             options=df['sleeve'].to_list(),
+                             value = 'Straight',
+                             style={'fontFamily': 'American Typewriter'}
+                            )
+    children += [sleeve_heading, sleeve_label, sleeve_dd]
+    
+
+    # Embellishments
+    # Dropdown menu for embellishments
+    embellish_heading = html.H2('Embellishments',
+                           style={'fontFamily': 'American Typewriter',
+                                  'background-color': 'LavenderBlush',
+                                  'textAlign': 'center'}
+                            )
+    embellish_label = html.H4('Select any optional embellishments:',
+                              id='embellish-label',
+                              style={'fontFamily': 'American Typewriter'}
+                            )
+    embellish_dd = dcc.Dropdown(id='embellish-dd',
+                             options=df['embellish'].to_list(),
+                             value='No embellishment',
+                             style={'fontFamily': 'American Typewriter'}
+                             )
+    children += [embellish_heading, embellish_label, embellish_dd]
+    
+    # Ribbing pattern:
+    ribbing_label = html.H4('Insert ribbing pattern (ex. 1x1, 1x2, 2x2):',
+                            id='ribbing-label',
+                            style={'fontFamily': 'American Typewriter'}
+                            )
+    ribbing_input1 = dcc.Input(id='rib-input1',
+                                type='number',
+                                placeholder='First number',
+                                style={'fontFamily': 'American Typewriter'}
+                                )
+                            
+    ribbing_input2 = dcc.Input(id='rib-input2',
+                               type='number',
+                               placeholder='Second number',
+                               style={'fontFamily': 'American Typewriter'}
+                                )
+    
+    children += [ribbing_label, ribbing_input1, ribbing_input2]
+    
+    # Cuff inputs:
+    cuff_header = html.H4('Enter your desired ribbed cuff height (in inches):',
+                            id='cuff-label',
+                            style={'fontFamily': 'American Typewriter'}
+                            )
+    cuff_input = dcc.Input(id='cuff-input',
+                            type='number',
+                            placeholder='Cuff height (in inches)',
+                            style={'display': 'block',
+                                'verticalAlign': 'top',
+                                'fontFamily': 'American Typewriter'}
+                            )
+    collar_header = html.H4('Enter your desired ribbed collar height (in inches):',
+                            id='collar-label',
+                            style={'fontFamily': 'American Typewriter'}
+                            )
+    collar_input = dcc.Input(id='collar-input',
+                            type='number',
+                            placeholder='Collar height (in inches)',
+                            style={'fontFamily': 'American Typewriter'}
+                            )
+    children += [cuff_header, cuff_input, collar_header, collar_input]
+    
+    # Add all embellishment inputs to the children list
+    
+    # SIZING
+    # Dropdown menu for sizing
+    size_heading = html.H2('Sizing',
+                           style={'fontFamily': 'American Typewriter',
+                                  'background-color': 'LavenderBlush',
+                                  'textAlign': 'center'}
+                            )
+    size_label = html.H4('Select the size of your sweater:',
+                         id='size-label',
+                         style = {'fontFamily': 'American Typewriter'}
+                        )
+    size_options = dcc.Dropdown(id='size-dd',
+                             options=df['size'].to_list()+['Insert own measurements'],
+                             value='Small',
+                             style={'fontFamily': 'American Typewriter'}
+                             )
+    
+    # Inputs for sizing
+    sizing_label = html.H4('Insert your desired sweater measurements in inches:',
+                            id='sizing-label',
+                            style={'fontFamily': 'American Typewriter'}
+                            )
+    size_input1 = dcc.Input(id='size-input1',
+                            type='number',
+                            placeholder='Insert sleeve length',
+                            style={'fontFamily': 'American Typewriter'}
+                            )
+    size_input2 = dcc.Input(id='size-input2',
+                            type='number',
+                            placeholder='Insert sleeve cuff width',
+                            style={'fontFamily': 'American Typewriter'}
+                            )
+    size_input3 = dcc.Input(id='size-input3',
+                            type='number',
+                            placeholder='Insert shoulder width',
+                            style={'fontFamily': 'American Typewriter'}
+                            )
+    size_input4 = dcc.Input(id='size-input4',
+                            type='number',
+                            placeholder='Insert body height',
+                            style={'fontFamily': 'American Typewriter'}
+                            )
+    size_input5 = dcc.Input(id='size-input5',
+                            type='number',
+                            placeholder='Insert bottom width',
+                            style={'fontFamily': 'American Typewriter'}
+                            )
+    size_input6 = dcc.Input(id='size-input6',
+                            type='number',
+                            placeholder='Insert neck opening width',
+                            style={'fontFamily': 'American Typewriter'}
+                            )
+    children += [size_heading, size_label, size_options, sizing_label,
+                 size_input1, size_input2, size_input3,
+                 size_input4, size_input5, size_input6]
+    
+    # COLUMN 2
+    # Image of the sweater
+    children1 = []
+    image = html.Img(id='image',
+                    src=app.get_asset_url('assets/drop/drop_crew_straight.jpg'),
+                    style={'width': '100%', 'height': 'auto',
+                        'display': 'inline-block', 'verticalAlign': 'top'}
+                    )
+    
+    # GAUGE
+    gauge_heading = html.H2('Gauge',
+                           style={'fontFamily': 'American Typewriter',
+                                'background-color': 'LavenderBlush',
+                                'textAlign': 'center'}
+                            )
+    gauge_label = html.H4('Enter the gauge of your chosen yarn in inches:',
+                          id='gauge-label',
+                          style={'fontFamily': 'American Typewriter'}
+                        )
+    length = html.Div([
+        html.H4('Insert the length of your swatch:',
+                id='length-label',
+                style={'fontFamily': 'American Typewriter'}
+                ),
+        dcc.Input(id='length-input',
+                type='number',
+                placeholder='Insert length of swatch',
+                style={'fontFamily': 'American Typewriter'}
+                )
+        ], style={'display': 'flex', 'alignItems': 'center'})
+    
+    stitch = html.Div([
+        html.H4('Insert the number of stitches per inserted length:',
+                id='stitch-label',
+                style={'fontFamily': 'American Typewriter'}
+                ),
+        dcc.Input(id='stitch-input',
+                type='number',
+                placeholder='Insert number of stitches',
+                style={'fontFamily': 'American Typewriter'}
+                )
+        ], style={'display': 'flex', 'alignItems': 'center'})
+    
+    height = html.Div([
+        html.H4('Insert the height of your swatch:',
+                id='height-label',
+                style={'fontFamily': 'American Typewriter'}
+                ),
+        dcc.Input(id='height-input',
+                type='number',
+                placeholder='Insert height of swatch',
+                style={'fontFamily': 'American Typewriter'}
+                )
+        ], style={'display': 'flex', 'alignItems': 'center'})
+    
+    row = html.Div([
+        html.H4('Insert the number of rows per inserted height:',
+                id='row-label',
+                style={'fontFamily': 'American Typewriter'}
+                ),
+        dcc.Input(id='row-input',
+                type='number',
+                placeholder='Insert number of rows',
+                style={'fontFamily': 'American Typewriter'}
+                )
+        ], style={'display': 'flex', 'alignItems': 'center'})
+    
+    button = html.Button('Generate Pattern', id='generate-btn', n_clicks=0)
+
+    link = html.A('Download Pattern PDF',
+                    id='download-link',
+                    href='',
+                    download='sweater_pattern.pdf',
+                    target='_blank',
+                    style={'display': 'none',
+                           'marginTop': '20px',
+                           'fontSize': '12px',
+                           'fontFamily': 'American Typewriter'})
+
+    
+    children1 += [image, gauge_heading, gauge_label, length, stitch, height, row, button, link]
+
+    # LAYOUT
+    app.layout = html.Div([
+        header,
+        name_label,
+        name_input,
+        html.Hr(),
+        html.Div([
+            html.Div(children=children, style={'flex': '1', 'padding': '10px'}),
+            html.Div(children=children1, style={'flex': '1', 'padding': '10px', 'width': '100%'})
+            ], style={'display': 'flex'})
+        ])
+       
+    return
+
+@callback(
+    Output('image','src'),
+    Input('sleeve-dd', 'value'),
+    Input('neck-dd', 'value'),
+    Input('embellish-dd', 'value')
+)
+def update_image(sleeve: str,
+                 neck: str,
+                 embellish: str,
+                 ):
+    neck_opts = {
+        'Boat neck': '_boat',
+        'Crew neck': '_crew',
+        'Mock neck': '_mock',
+        'Turtleneck': '_turtle'
+    }
+    sleeve_opts = {
+        'Straight': '_straight',
+        'Tapered': '_tapered',
+        'Flare': '_flare',
+        'Balloon': '_balloon'
+    }
+    embellish_opts = {
+        'Ribbed edges': '_cuff',
+        'Ribbed collar': '_collar',
+        'Ribbed edges and collar': '_cuff_collar',
+        'No embellishment': ''
+    }
+
+    img = Image.open(f'assets/drop{neck_opts[neck]}{sleeve_opts[sleeve]}{embellish_opts[embellish]}.jpg')
+    return img
+    
+@callback(
+    Output('ribbing-label', 'style'),
+    Output('rib-input1', 'style'),
+    Output('rib-input2', 'style'),
+    Output('cuff-label', 'style'),
+    Output('cuff-input', 'style'),
+    Input('embellish-dd', 'value'),
+)
+def update_embellish_input(embellish: str):
+    if embellish != 'No embellishment':
+        style = {'display': 'block',
+                 'fontFamily': 'American Typewriter'
+                 }
+    else:
+        style = {'display': 'none',
+                 'fontFamily': 'American Typewriter',
+                 }
+    return style, style, style, style, style
+    
+@callback(
+    Output('collar-label', 'style'),
+    Output('collar-input', 'style'),
+    Input('neck-dd', 'value'),
+    Input('embellish-dd', 'value')
+)
+def update_collar(neck: str, embellish: str):
+    if (neck == 'Turtleneck' or 'Mock neck') or (embellish == 'No embellishment'):
+        style = {'display': 'none',
+                 'fontFamily': 'American Typewriter'
+                 }
+    else:
+        style = {'display': 'block',
+                 'fontFamily': 'American Typewriter',
+                 }
+    return style, style
+
+@callback(
+    Output('sizing-label', 'style'),
+    Output('size-input1', 'style'),
+    Output('size-input2', 'style'),
+    Output('size-input3', 'style'),
+    Output('size-input4', 'style'),
+    Output('size-input5', 'style'),
+    Output('size-input6', 'style'),
+    Input('size-dd', 'value')
+)
+def update_size_input(size: str):
+    if size == 'Insert own measurements':
+        style = {'display': 'block',
+                 'fontFamily': 'American Typewriter'
+                 }
+    else:
+        style = {'display': 'none',
+                 'fontFamily': 'American Typewriter',
+                 }
+    return style, style, style, style, style, style, style
+    
+
+@callback(
+    Output('download-link', 'href'),
+    Output('download-link', 'style'),
+    Input('generate-btn', 'n_clicks'),
+    Input('name-input', 'value'),
+    Input('neck-dd', 'value'),
+    Input('sleeve-dd', 'value'),
+    Input('embellish-dd', 'value'),
+    Input('rib-input1', 'value'),
+    Input('rib-input2', 'value'),
+    Input('cuff-input', 'value'),
+    Input('collar-input', 'value'),
+    Input('size-dd', 'value'),
+    Input('size-input1', 'value'),
+    Input('size-input2', 'value'),
+    Input('size-input3', 'value'),
+    Input('size-input4', 'value'),
+    Input('size-input5', 'value'),
+    Input('size-input6', 'value'),
+    Input('length-input', 'value'),
+    Input('stitch-input', 'value'),
+    Input('height-input', 'value'),
+    Input('row-input', 'value')
+    )
+def generate_pattern(n_clicks, name,
+                    neck, sleeve, embellishment,
+                    rib1, rib2, cuff, collar, size,
+                    sleeve_length, sleeve_cuff_width, shoulder_width,
+                    body_height, bottom_width, neck_opening_width,
+                    length, stitch, height, row):
+    if n_clicks == 0:
+        return '', {'display': 'none'}
+    
+    try:
+        pattern = SweaterPattern(name, neck, sleeve, embellishment,
+                            rib1, rib2, cuff, collar, size,
+                            sleeve_length, sleeve_cuff_width, shoulder_width,
+                            body_height, bottom_width, neck_opening_width,
+                            length, stitch, height, row)
+        filename = pattern.generate_pdf()
+        href = f"/download/{filename}"
+        return href, {'display': 'inline'}
+    except Exception as e:
+        print(f"Error: {e}")
+        return '', {'display': 'none'}
